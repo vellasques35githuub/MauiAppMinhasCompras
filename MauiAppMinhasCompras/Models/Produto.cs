4f35b48a4e8764b13acbca34747cf2dea06b06b3
fabio <fabio@DESKTOP-C274NH0>
2025-03-14 14:56:45 -0300

    Construção e montagem do botão soma e listagem de produtos

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -17,5 +17,7 @@ namespace MauiAppMinhasCompras.Models
         public double Quantidade { get; set; }
 
         public double Preco { get; set; }
+
+        public double Total { get => Quantidade * Preco; }
     }
 }
